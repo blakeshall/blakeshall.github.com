ee87a33ad6a10939498cfcc2b3be6b7ba741bc29
Blake Hall <blake@lexington.home>
2024-02-05 15:04:49 -0500

    Update

--- js/offcanvas.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('[data-toggle=offcanvas]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-    $('.pull').toggleClass('glyphicon-chevron-right');
-    $('.pull').toggleClass('glyphicon-chevron-left');
-  });
-});
